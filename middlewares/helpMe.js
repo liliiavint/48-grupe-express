5a7bb2914ed4f503f3616effdc9c3112a76f10f1
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-12 12:46:05 +0200

    new middlewares

--- middlewares/helpMe.js
+++ middlewares/helpMe.js
@@ -1,12 +1,19 @@
 let requestCount = 0;
 
-function helpMe(req, res, next) {
-    req.timeItHasHappened = Date.now();
+// function helpMe(req, res, next) {
+//     req.timeItHasHappened = Date.now().toLocaleDateString();
 
+//     requestCount++;
+//     console.log(req.timeItHasHappened, '>', requestCount, req.url);
+
+//     next();
+// }
+function helpMe(req, res, next) {
+    req.timeItHasHappened = new Date().toLocaleString(); // Change to current date
+    
     requestCount++;
-    console.log(req.timeItHasHappened, '>', requestCount, req.url);
+    console.log(req.timeItHasHappened, '>', requestCount, req.url, req.ip);
 
     next();
 }
-
 export { helpMe };
